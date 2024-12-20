89ad203250b69c83605a72594c2699da6e7c8453
haderiumtech <haderiumtechnologies@gmail.com>
2024-12-21 00:47:44 +0500

    changes in excelutils

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -9,43 +9,32 @@ import utils.RetryAnalyzer;
 
 public class LoginTest extends BaseClass {
 
-	LoginXpaths dMUKXp = new LoginXpaths();
+	LoginXpaths demoXp = new LoginXpaths();
 
 	@Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
-	public void homePageTitleVerification() throws InterruptedException {
-		// Create the test case in ExtentReports
-		test = extent.createTest("Test case for verifying homepage");
-
+	public void loginTestCase() throws InterruptedException {
+		
+		test = extent.createTest("Test case for login functionality");
+		
 		try {
-
-			LoggerUtils.logInfo("Starting the test case: homePageTitleVerification");
-
-			String pageTitle = getTitle();
-			LoggerUtils.logInfo("Current page title: " + pageTitle);
-
-		// Verify the title using the page's title and the expected title from the xpaths
-			verifyText(dMUKXp.homeTitle, pageTitle);
-			LoggerUtils.logInfo("Test Passed: homepage title verification successful.");
-
-		} catch (AssertionError e) {
-			// Log any assertion failure
+		super.sendKeys(demoXp.usernmae, demoXp.usernameData);
+		super.sendKeys(demoXp.password, demoXp.passData);
+		super.forClick(demoXp.loginButton);
+		
+		String pageTitle = super.getTitle();
+		verifyText(demoXp.homeTitle, pageTitle);
+		
+		
+		}catch (AssertionError e) {
 			LoggerUtils.logError("Test Failed: homepage title verification failed.");
 			LoggerUtils.logError("Error: " + e.getMessage());
-			throw e; // Rethrow the exception to ensure TestNG captures it as a failure
+			throw e;
 			
 		} catch (Exception e) {
-			// Log any other exceptions
 			LoggerUtils.logError("An error occurred while verifying the homepage title.");
 			LoggerUtils.logError("Error: " + e.getMessage());
-			throw e; // Rethrow the exception to ensure TestNG captures it as a failure
+			throw e; 
 		}
-
 	}
-
-//	@Test(priority = 2)
-//	public void searchBarTestCase() throws InterruptedException {
-//		test = extent.createTest("Test case for searching functionality");
-//
-//	}
 
 }
